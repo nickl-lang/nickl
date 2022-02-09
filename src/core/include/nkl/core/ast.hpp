eab4a4a54e45b95453c1ccfa60b9bc7993582cb4
Nick Karter <nkarter@protonmail.com>
2022-02-09 19:33:10 +0200

    Shrink AST to IR scope

--- src/core/include/nkl/core/ast.hpp
+++ src/core/include/nkl/core/ast.hpp
@@ -12,7 +12,7 @@ enum ENodeId {
     Node_count,
 };
 
-extern char const *s_node_names[];
+extern char const *s_ast_node_names[];
 
 struct Node;
 using node_ref_t = Node const *;
@@ -105,7 +105,6 @@ struct Node {
         struct _binary binary;
         struct _call call;
         struct _fn fn;
-        struct _fn_type fn_type;
         struct _id id;
         struct _member member;
         struct _numeric numeric;
@@ -118,7 +117,7 @@ struct Node {
 
         NamedNode named_node;
     } as;
-    Id id;
+    size_t id;
 };
 
 struct Ast {
@@ -127,93 +126,10 @@ struct Ast {
     void init();
     void deinit();
 
-#define NULLARY_NODE(NAME) Node make_##NAME();
-
-    NULLARY_NODE(any)
-    NULLARY_NODE(bool)
-    NULLARY_NODE(break)
-    NULLARY_NODE(continue)
-    NULLARY_NODE(f32)
-    NULLARY_NODE(f64)
-    NULLARY_NODE(false)
-    NULLARY_NODE(i16)
-    NULLARY_NODE(i32)
-    NULLARY_NODE(i64)
-    NULLARY_NODE(i8)
-    NULLARY_NODE(nop)
-    NULLARY_NODE(string)
-    NULLARY_NODE(symbol)
-    NULLARY_NODE(true)
-    NULLARY_NODE(typeref)
-    NULLARY_NODE(u16)
-    NULLARY_NODE(u32)
-    NULLARY_NODE(u64)
-    NULLARY_NODE(u8)
-    NULLARY_NODE(void)
-
-#undef NULLARY_NODE
-
-#define UNARY_NODE(NAME) Node make_##NAME(node_ref_t arg);
-
-    UNARY_NODE(addr)
-    UNARY_NODE(alignof)
-    UNARY_NODE(assert)
-    UNARY_NODE(compl )
-    UNARY_NODE(const)
-    UNARY_NODE(dec)
-    UNARY_NODE(deref)
-    UNARY_NODE(inc)
-    UNARY_NODE(not )
-    UNARY_NODE(pdec)
-    UNARY_NODE(pinc)
-    UNARY_NODE(return )
-    UNARY_NODE(sizeof)
-    UNARY_NODE(typeof)
-    UNARY_NODE(uminus)
-    UNARY_NODE(uplus)
-
-#undef UNARY_NODE
-
-#define BINARY_NODE(NAME) Node make_##NAME(node_ref_t lhs, node_ref_t rhs);
-
-    BINARY_NODE(add)
-    BINARY_NODE(add_assign)
-    BINARY_NODE(and)
-    BINARY_NODE(and_assign)
-    BINARY_NODE(array_type)
-    BINARY_NODE(assign)
-    BINARY_NODE(bitand)
-    BINARY_NODE(bitand_assign)
-    BINARY_NODE(bitor)
-    BINARY_NODE(bitor_assign)
-    BINARY_NODE(cast)
-    BINARY_NODE(colon_assign)
-    BINARY_NODE(div)
-    BINARY_NODE(div_assign)
-    BINARY_NODE(eq)
-    BINARY_NODE(ge)
-    BINARY_NODE(gt)
-    BINARY_NODE(index)
-    BINARY_NODE(le)
-    BINARY_NODE(lsh)
-    BINARY_NODE(lsh_assign)
-    BINARY_NODE(lt)
-    BINARY_NODE(mod)
-    BINARY_NODE(mod_assign)
-    BINARY_NODE(mul)
-    BINARY_NODE(mul_assign)
-    BINARY_NODE(ne)
-    BINARY_NODE(or)
-    BINARY_NODE(or_assign)
-    BINARY_NODE(rsh)
-    BINARY_NODE(rsh_assign)
-    BINARY_NODE(sub)
-    BINARY_NODE(sub_assign)
-    BINARY_NODE(while)
-    BINARY_NODE(xor)
-    BINARY_NODE(xor_assign)
-
-#undef BINARY_NODE
+#define N(TYPE, ID) Node make_##ID();
+#define U(TYPE, ID) Node make_##ID(node_ref_t arg);
+#define B(TYPE, ID) Node make_##ID(node_ref_t lhs, node_ref_t rhs);
+#include "nkl/core/nodes.inl"
 
     Node make_if(node_ref_t cond, node_ref_t then_clause, node_ref_t else_clause);
     Node make_ternary(node_ref_t cond, node_ref_t then_clause, node_ref_t else_clause);
@@ -224,10 +140,8 @@ struct Ast {
     Node make_tuple_type(NodeArray nodes);
 
     Node make_id(Id name);
-    Node make_sym(Id name);
 
     Node make_member(node_ref_t lhs, Id name);
-    Node make_offsetof(node_ref_t lhs, Id name);
 
     Node make_numeric_i8(int8_t val);
     Node make_numeric_i16(int16_t val);
@@ -241,12 +155,10 @@ struct Ast {
     Node make_numeric_f64(double val);
 
     Node make_struct(Id name, NamedNodeArray fields);
-    Node make_union(Id name, NamedNodeArray fields);
 
     Node make_call(node_ref_t lhs, NodeArray args);
     Node make_fn(Id name, NamedNodeArray params, node_ref_t ret_type, node_ref_t body);
-    Node make_fn_type(Id name, NamedNodeArray params, node_ref_t ret_type);
-    Node make_string_literal(Allocator *allocator, string str);
+    Node make_string_literal(Allocator &allocator, string str);
     Node make_struct_literal(node_ref_t type, NamedNodeArray fields);
     Node make_var_decl(Id name, node_ref_t type, node_ref_t value);
 
@@ -255,7 +167,7 @@ struct Ast {
     NodeArray push_named_ar(NamedNodeArray nodes);
 };
 
-string ast_inspect(Allocator *allocator, node_ref_t node);
+string ast_inspect(node_ref_t node);
 
 static node_ref_t const n_none_ref = nullptr;
 
--- src/core/test/ast_test.cpp
+++ src/core/test/ast_test.cpp
@@ -9,31 +9,31 @@
 #include "nk/common/arena.hpp"
 #include "nk/common/logger.hpp"
 #include "nk/common/utils.hpp"
+#include "nk/vm/vm.hpp"
 
 namespace {
 
-LOG_USE_SCOPE(test)
+using namespace nk;
+
+LOG_USE_SCOPE(nkl::ast::test)
 
 class ast : public testing::Test {
     void SetUp() override {
         LOGGER_INIT(LoggerOptions{});
 
-        m_arena.init();
-        id_init();
+        vm::vm_init({});
         m_ast.init();
     }
 
     void TearDown() override {
         m_ast.deinit();
-        id_deinit();
-        m_arena.deinit();
+        vm::vm_deinit();
     }
 
 protected:
     void test(node_ref_t node, Id id) {
-        string str = id2s(id);
-        LOG_INF("Test for #%.*s", str.size, str.data);
-        str = ast_inspect(&m_arena, node);
+        LOG_INF("Test for #%s", s_ast_node_names[id]);
+        auto str = ast_inspect(node);
         LOG_INF("%.*s", str.size, str.data);
         m_node = node;
         ASSERT_TRUE(m_node);
@@ -145,7 +145,6 @@ protected:
                 m_ast.push(m_ast.make_id(cs2id("a"))), m_ast.push(m_ast.make_id(cs2id("b")))))))));
     }
 
-    ArenaAllocator m_arena;
     Ast m_ast;
     node_ref_t m_node;
 };
@@ -158,79 +157,14 @@ TEST_F(ast, nodes) {
     Id const noid = cs2id("noid");
     NamedNode const nn_nop{noid, nop};
 
-    test(nop, Node_nop);
+#define N(TYPE, ID) test(m_ast.push(m_ast.make_##ID()), Node_##ID);
+#include "nkl/core/nodes.inl"
 
-    test(m_ast.push(m_ast.make_any()), Node_any);
-    test(m_ast.push(m_ast.make_bool()), Node_bool);
-    test(m_ast.push(m_ast.make_break()), Node_break);
-    test(m_ast.push(m_ast.make_continue()), Node_continue);
-    test(m_ast.push(m_ast.make_f32()), Node_f32);
-    test(m_ast.push(m_ast.make_f64()), Node_f64);
-    test(m_ast.push(m_ast.make_false()), Node_false);
-    test(m_ast.push(m_ast.make_i16()), Node_i16);
-    test(m_ast.push(m_ast.make_i32()), Node_i32);
-    test(m_ast.push(m_ast.make_i64()), Node_i64);
-    test(m_ast.push(m_ast.make_i8()), Node_i8);
-    test(m_ast.push(m_ast.make_string()), Node_string);
-    test(m_ast.push(m_ast.make_symbol()), Node_symbol);
-    test(m_ast.push(m_ast.make_true()), Node_true);
-    test(m_ast.push(m_ast.make_typeref()), Node_typeref);
-    test(m_ast.push(m_ast.make_u16()), Node_u16);
-    test(m_ast.push(m_ast.make_u32()), Node_u32);
-    test(m_ast.push(m_ast.make_u64()), Node_u64);
-    test(m_ast.push(m_ast.make_u8()), Node_u8);
-    test(m_ast.push(m_ast.make_void()), Node_void);
+#define U(TYPE, ID) test_unary(m_ast.push(m_ast.make_##ID(nop)), Node_##ID, nop);
+#include "nkl/core/nodes.inl"
 
-    test_unary(m_ast.push(m_ast.make_addr(nop)), Node_addr, nop);
-    test_unary(m_ast.push(m_ast.make_alignof(nop)), Node_alignof, nop);
-    test_unary(m_ast.push(m_ast.make_assert(nop)), Node_assert, nop);
-    test_unary(m_ast.push(m_ast.make_compl(nop)), Node_compl, nop);
-    test_unary(m_ast.push(m_ast.make_dec(nop)), Node_dec, nop);
-    test_unary(m_ast.push(m_ast.make_deref(nop)), Node_deref, nop);
-    test_unary(m_ast.push(m_ast.make_inc(nop)), Node_inc, nop);
-    test_unary(m_ast.push(m_ast.make_not(nop)), Node_not, nop);
-    test_unary(m_ast.push(m_ast.make_pdec(nop)), Node_pdec, nop);
-    test_unary(m_ast.push(m_ast.make_pinc(nop)), Node_pinc, nop);
-    test_unary(m_ast.push(m_ast.make_return(nop)), Node_return, nop);
-    test_unary(m_ast.push(m_ast.make_sizeof(nop)), Node_sizeof, nop);
-    test_unary(m_ast.push(m_ast.make_typeof(nop)), Node_typeof, nop);
-    test_unary(m_ast.push(m_ast.make_uminus(nop)), Node_uminus, nop);
-    test_unary(m_ast.push(m_ast.make_uplus(nop)), Node_uplus, nop);
-
-    test_binary(m_ast.push(m_ast.make_add_assign(nop, nop)), Node_add_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_add(nop, nop)), Node_add, nop, nop);
-    test_binary(m_ast.push(m_ast.make_and_assign(nop, nop)), Node_and_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_and(nop, nop)), Node_and, nop, nop);
-    test_binary(m_ast.push(m_ast.make_array_type(nop, nop)), Node_array_type, nop, nop);
-    test_binary(m_ast.push(m_ast.make_assign(nop, nop)), Node_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_bitand(nop, nop)), Node_bitand, nop, nop);
-    test_binary(m_ast.push(m_ast.make_bitor(nop, nop)), Node_bitor, nop, nop);
-    test_binary(m_ast.push(m_ast.make_cast(nop, nop)), Node_cast, nop, nop);
-    test_binary(m_ast.push(m_ast.make_colon_assign(nop, nop)), Node_colon_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_div_assign(nop, nop)), Node_div_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_div(nop, nop)), Node_div, nop, nop);
-    test_binary(m_ast.push(m_ast.make_eq(nop, nop)), Node_eq, nop, nop);
-    test_binary(m_ast.push(m_ast.make_ge(nop, nop)), Node_ge, nop, nop);
-    test_binary(m_ast.push(m_ast.make_gt(nop, nop)), Node_gt, nop, nop);
-    test_binary(m_ast.push(m_ast.make_index(nop, nop)), Node_index, nop, nop);
-    test_binary(m_ast.push(m_ast.make_le(nop, nop)), Node_le, nop, nop);
-    test_binary(m_ast.push(m_ast.make_lsh_assign(nop, nop)), Node_lsh_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_lsh(nop, nop)), Node_lsh, nop, nop);
-    test_binary(m_ast.push(m_ast.make_lt(nop, nop)), Node_lt, nop, nop);
-    test_binary(m_ast.push(m_ast.make_mod_assign(nop, nop)), Node_mod_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_mod(nop, nop)), Node_mod, nop, nop);
-    test_binary(m_ast.push(m_ast.make_mul_assign(nop, nop)), Node_mul_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_mul(nop, nop)), Node_mul, nop, nop);
-    test_binary(m_ast.push(m_ast.make_ne(nop, nop)), Node_ne, nop, nop);
-    test_binary(m_ast.push(m_ast.make_or_assign(nop, nop)), Node_or_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_or(nop, nop)), Node_or, nop, nop);
-    test_binary(m_ast.push(m_ast.make_rsh_assign(nop, nop)), Node_rsh_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_rsh(nop, nop)), Node_rsh, nop, nop);
-    test_binary(m_ast.push(m_ast.make_sub_assign(nop, nop)), Node_sub_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_sub(nop, nop)), Node_sub, nop, nop);
-    test_binary(m_ast.push(m_ast.make_while(nop, nop)), Node_while, nop, nop);
-    test_binary(m_ast.push(m_ast.make_xor_assign(nop, nop)), Node_xor_assign, nop, nop);
-    test_binary(m_ast.push(m_ast.make_xor(nop, nop)), Node_xor, nop, nop);
+#define B(TYPE, ID) test_binary(m_ast.push(m_ast.make_##ID(nop, nop)), Node_##ID, nop, nop);
+#include "nkl/core/nodes.inl"
 
     test_ternary(m_ast.push(m_ast.make_if(nop, nop, nop)), Node_if, nop, nop, nop);
     test_ternary(m_ast.push(m_ast.make_ternary(nop, nop, nop)), Node_ternary, nop, nop, nop);
@@ -245,17 +179,10 @@ TEST_F(ast, nodes) {
         Node_struct,
         noid,
         {noid, nop});
-    test_type_decl(
-        m_ast.push(m_ast.make_union(noid, NamedNodeArray{&nn_nop, 1})),
-        Node_union,
-        noid,
-        {noid, nop});
 
     test_member(m_ast.push(m_ast.make_member(nop, noid)), Node_member, nop, noid);
-    test_member(m_ast.push(m_ast.make_offsetof(nop, noid)), Node_offsetof, nop, noid);
 
     test_id(m_ast.push(m_ast.make_id(noid)), Node_id, noid);
-    test_id(m_ast.push(m_ast.make_sym(noid)), Node_sym, noid);
 
     test(m_ast.push(m_ast.make_numeric_f64(3.14)), Node_numeric_f64);
     EXPECT_EQ(m_node->as.numeric.val.f64, 3.14);
@@ -281,7 +208,9 @@ TEST_F(ast, nodes) {
     EXPECT_EQ(m_node->as.fn.sig.ret_type, nop);
     EXPECT_EQ(m_node->as.fn.body, nop);
 
-    test(m_ast.push(m_ast.make_string_literal(&m_arena, cs2s("hello"))), Node_string_literal);
+    test(
+        m_ast.push(m_ast.make_string_literal(*_mctx.tmp_allocator, cs2s("hello"))),
+        Node_string_literal);
     EXPECT_EQ(std_view(m_node->as.str.val), "hello");
 
     test(
@@ -291,12 +220,6 @@ TEST_F(ast, nodes) {
     EXPECT_EQ(m_node->as.struct_literal.fields.size, 1);
     EXPECT_EQ(m_node->as.struct_literal.fields.data->as.named_node.name, noid);
     EXPECT_EQ(m_node->as.struct_literal.fields.data->as.named_node.node, nop);
-
-    test(m_ast.push(m_ast.make_fn_type(noid, NamedNodeArray{&nn_nop, 1}, nop)), Node_fn_type);
-    EXPECT_EQ(m_node->as.fn_type.name, noid);
-    EXPECT_EQ(m_node->as.fn_type.params.size, 1);
-    EXPECT_EQ(m_node->as.fn_type.params.data->as.named_node.name, noid);
-    EXPECT_EQ(m_node->as.fn_type.params.data->as.named_node.node, nop);
 }
 
 TEST_F(ast, to_source) {
@@ -306,7 +229,7 @@ TEST_F(ast, to_source) {
 }
 
 TEST_F(ast, inspect) {
-    auto str = ast_inspect(&m_arena, makeTestTree());
+    auto str = ast_inspect(makeTestTree());
     ASSERT_NE(str.size, 0);
     LOG_INF("%.*s", str.size, str.data);
 }
