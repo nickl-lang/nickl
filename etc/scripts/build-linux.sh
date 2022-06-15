766a41204a1f719628a83aa1d2708c428576258e
Nick Karter <nkarter@protonmail.com>
2022-06-15 11:09:20 +0300

    Add linux buildenv

--- etc/scripts/build-linux.sh
+++ etc/scripts/build-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PLATFORM=linux
 DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
-PROJ_ROOT="$DIR/../.."
-BIN_DIR="$PROJ_ROOT/out/build-$PLATFORM"
-cmake -S $PROJ_ROOT -B $BIN_DIR -GNinja
-ninja -C $BIN_DIR
+PROJ_ROOT=$(realpath $DIR/../..)
+BIN_DIR=$(realpath $PROJ_ROOT/out/build-$PLATFORM)
+$PROJ_ROOT/etc/buildenv/$PLATFORM/run-docker.sh cmake -S $PROJ_ROOT -B $BIN_DIR -GNinja
+$PROJ_ROOT/etc/buildenv/$PLATFORM/run-docker.sh ninja -C $BIN_DIR
