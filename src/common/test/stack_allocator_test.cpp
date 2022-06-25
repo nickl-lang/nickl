12c46b96da3e6aa523e442005a494d33e0981dcc
Nick Karter <nkarter@protonmail.com>
2022-06-25 15:42:21 +0000

    Add StackAllocator test

--- src/common/test/stack_allocator_test.cpp
+++ src/common/test/stack_allocator_test.cpp
@@ -1,4 +1,6 @@
+#define private public
 #include "nk/common/stack_allocator.hpp"
+#undef private
 
 #include <gtest/gtest.h>
 
@@ -23,4 +25,31 @@ TEST_F(stack_allocator, basic) {
     *ptr = 'a';
 }
 
-/// @TODO Add more StackAllocator tests
+TEST_F(stack_allocator, reserve) {
+    EXPECT_EQ(m_allocator.m_arena._first_block, nullptr);
+    m_allocator.reserve(1000);
+    EXPECT_NE(m_allocator.m_arena._first_block, nullptr);
+}
+
+TEST_F(stack_allocator, frames) {
+    m_allocator.alloc<uint8_t>(100);
+    EXPECT_EQ(m_allocator.m_arena.size, 100);
+
+    auto frame = m_allocator.pushFrame();
+
+    m_allocator.alloc<uint8_t>(100);
+    EXPECT_EQ(m_allocator.m_arena.size, 200);
+
+    m_allocator.popFrame(frame);
+    EXPECT_EQ(m_allocator.m_arena.size, 100);
+}
+
+TEST_F(stack_allocator, align) {
+    struct A {
+        uint8_t a;
+        std::max_align_t b;
+    };
+
+    auto ptr = m_allocator.alloc<A>();
+    EXPECT_EQ((size_t)ptr % alignof(A), 0);
+}
