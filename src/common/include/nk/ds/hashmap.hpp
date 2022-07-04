c8545baa73eef64fdad889c6f1964215a59c9f21
Nick Karter <nkarter@protonmail.com>
2022-07-04 19:43:49 +0000

    Add one more HashSet test

--- src/common/include/nk/ds/hashmap.hpp
+++ src/common/include/nk/ds/hashmap.hpp
@@ -17,6 +17,57 @@ using DefaultHashMapContext = DefaultHashSetContext<K>;
 
 template <class K, class V, class Context = detail::DefaultHashMapContext<K>>
 struct HashMap {
+    void reserve(size_t cap) {
+        m_entries.reserve(cap);
+    }
+
+    void deinit() {
+        m_entries.deinit();
+    }
+
+    size_t size() const {
+        return m_entries.size;
+    }
+
+    size_t capacity() const {
+        return m_entries.capacity;
+    }
+
+    bool insert(K const &key, V const &value) {
+        EASY_BLOCK("HashMap::insert", profiler::colors::Grey200)
+        //@Robustness Creating a default value for find in HashMap
+        _Entry *found = m_entries.find(_Entry{key, {}});
+        if (found) {
+            found->value = value;
+        } else {
+            m_entries.insert(_Entry{key, value});
+        }
+        return !found;
+    }
+
+    V &operator[](K const &key) {
+        EASY_BLOCK("HashMap::operator[]", profiler::colors::Grey200)
+        _Entry *found = m_entries.find(_Entry{key, {}});
+        if (!found) {
+            m_entries.insert(_Entry{key, {}});
+            //@Robustness Finding again in HashMap::operator[]
+            found = m_entries.find(_Entry{key, {}});
+        }
+        return found->value;
+    }
+
+    V *find(K const &key) const {
+        EASY_BLOCK("HashMap::find", profiler::colors::Grey200)
+        _Entry *found = m_entries.find(_Entry{key, {}});
+        return found ? &found->value : nullptr;
+    }
+
+    void remove(K const &key) {
+        EASY_BLOCK("HashMap::remove", profiler::colors::Grey200)
+        m_entries.remove(_Entry{key, {}});
+    }
+
+private:
     struct _Entry {
         K key;
         V value;
@@ -32,57 +83,7 @@ struct HashMap {
         }
     };
 
-    HashSet<_Entry, _HashSetContext> entries;
-
-    void reserve(size_t cap) {
-        entries.reserve(cap);
-    }
-
-    void deinit() {
-        entries.deinit();
-    }
-
-    size_t size() const {
-        return entries.size;
-    }
-
-    size_t capacity() const {
-        return entries.capacity;
-    }
-
-    bool insert(K const &key, V const &value) {
-        EASY_BLOCK("HashMap::insert", profiler::colors::Grey200)
-        //@Robustness Creating a default value for find in HashMap
-        _Entry *found = entries.find(_Entry{key, {}});
-        if (found) {
-            found->value = value;
-        } else {
-            entries.insert(_Entry{key, value});
-        }
-        return !found;
-    }
-
-    V &operator[](K const &key) {
-        EASY_BLOCK("HashMap::operator[]", profiler::colors::Grey200)
-        _Entry *found = entries.find(_Entry{key, {}});
-        if (!found) {
-            entries.insert(_Entry{key, {}});
-            //@Robustness Finding again in HashMap::operator[]
-            found = entries.find(_Entry{key, {}});
-        }
-        return found->value;
-    }
-
-    V *find(K const &key) const {
-        EASY_BLOCK("HashMap::find", profiler::colors::Grey200)
-        _Entry *found = entries.find(_Entry{key, {}});
-        return found ? &found->value : nullptr;
-    }
-
-    void remove(K const &key) {
-        EASY_BLOCK("HashMap::remove", profiler::colors::Grey200)
-        entries.remove(_Entry{key, {}});
-    }
+    HashSet<_Entry, _HashSetContext> m_entries;
 };
 
 } // namespace nk
