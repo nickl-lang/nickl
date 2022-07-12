65dd9a172a955748c804f8a61fe098051ce9d804
Nick Karter <nkarter@protonmail.com>
2022-07-12 22:56:39 +0000

    Update some parser tests

--- src/lang/test/parser_test.cpp
+++ src/lang/test/parser_test.cpp
@@ -69,19 +69,12 @@ protected:
         }
     };
 
-    void test_ok(std::vector<TokenDescr> tokens, Node const &expected) {
-        StackAllocator arena{};
-
-        defer {
-            m_parser.ast.deinit();
-            arena.deinit();
-        };
-
+    bool test_ok(std::vector<TokenDescr> tokens) {
         LOG_INF("\n==================================================\nTest: %s", [&]() {
             for (auto const &token : tokens) {
                 m_sb << token.text << " ";
             }
-            auto str = m_sb.moveStr(arena);
+            auto str = m_sb.moveStr(m_arena);
             return str.data;
         }());
         tokens.emplace_back(t_eof, "");
@@ -92,29 +85,25 @@ protected:
         for (auto const &token : tokens) {
             *token_ar.push() = createToken(token.id, token.text);
         }
-        bool parse_ok = m_parser.parse(token_ar);
-        EXPECT_TRUE(parse_ok);
-        if (!parse_ok) {
-            auto const err_msg = m_parser.err.moveStr(arena);
-            LOG_ERR("Error message: %.*s", err_msg.size, err_msg.data);
-        }
+        return m_parser.parse(token_ar);
+    }
 
-        bool trees_are_equal = test::ast_equal(m_parser.root, &expected);
-        EXPECT_TRUE(trees_are_equal);
-        if (!trees_are_equal) {
-            m_sb << "\nActual tree: ";
-            ast_inspect(m_parser.root, m_sb);
-            m_sb << "\nExpected tree: ";
-            ast_inspect(&expected, m_sb);
+    void reportParseError() {
+        auto const err_msg = m_parser.err.moveStr(m_arena);
+        LOG_ERR("Error message: %.*s", err_msg.size, err_msg.data);
+    }
 
-            auto const msg = m_sb.moveStr(arena);
-            LOG_ERR("%.*s", msg.size, msg.data);
-        }
+    void reportTreeError(Node const &expected) {
+        m_sb << "\nActual tree: ";
+        ast_inspect(m_parser.root, m_sb);
+        m_sb << "\nExpected tree: ";
+        ast_inspect(&expected, m_sb);
+        auto const msg = m_sb.moveStr(m_arena);
+        LOG_ERR("%.*s", msg.size, msg.data);
     }
 
 protected:
     StackAllocator m_arena{};
-
     DynamicStringBuilder m_sb{};
     LangAst m_ast{};
     Parser m_parser{m_sb};
@@ -122,26 +111,49 @@ protected:
 
 } // namespace
 
+#define TEST_OK(EXPECTED, ...)                                                      \
+    {                                                                               \
+        bool const parse_ok = test_ok({__VA_ARGS__});                               \
+        EXPECT_TRUE(parse_ok);                                                      \
+        if (!parse_ok) {                                                            \
+            reportParseError();                                                     \
+        }                                                                           \
+        auto const &expected = EXPECTED;                                            \
+        bool trees_are_equal = test::ast_equal(m_parser.root, &expected);           \
+        EXPECT_TRUE(trees_are_equal)                                                \
+            << "Actual expr: " << #__VA_ARGS__ << "\nExpected expr: " << #EXPECTED; \
+        if (!trees_are_equal) {                                                     \
+            reportTreeError(expected);                                              \
+        }                                                                           \
+        m_parser.ast.deinit();                                                      \
+    }
+
 TEST_F(parser, empty) {
-    test_ok({}, m_ast.make_nop());
+    TEST_OK(m_ast.make_continue(), );
 }
 
 TEST_F(parser, nullary) {
-    test_ok({t_break, t_semi}, m_ast.make_break());
-    test_ok({t_continue, t_semi}, m_ast.make_continue());
-    test_ok({t_false, t_semi}, m_ast.make_false());
-    test_ok({t_true, t_semi}, m_ast.make_true());
-    test_ok({t_i16, t_semi}, m_ast.make_i16());
-    test_ok({t_i32, t_semi}, m_ast.make_i32());
-    test_ok({t_i64, t_semi}, m_ast.make_i64());
-    test_ok({t_i8, t_semi}, m_ast.make_i8());
-    test_ok({t_u16, t_semi}, m_ast.make_u16());
-    test_ok({t_u32, t_semi}, m_ast.make_u32());
-    test_ok({t_u64, t_semi}, m_ast.make_u64());
-    test_ok({t_u8, t_semi}, m_ast.make_u8());
-    test_ok({t_void, t_semi}, m_ast.make_void());
-    test_ok({t_f32, t_semi}, m_ast.make_f32());
-    test_ok({t_f64, t_semi}, m_ast.make_f64());
+    TEST_OK(m_ast.make_break(), t_break, t_semi);
+    TEST_OK(m_ast.make_continue(), t_continue, t_semi);
+
+    TEST_OK(m_ast.make_false(), t_false, t_semi);
+    TEST_OK(m_ast.make_true(), t_true, t_semi);
+
+    TEST_OK(m_ast.make_i8(), t_i8, t_semi);
+    TEST_OK(m_ast.make_i16(), t_i16, t_semi);
+    TEST_OK(m_ast.make_i32(), t_i32, t_semi);
+    TEST_OK(m_ast.make_i64(), t_i64, t_semi);
+    TEST_OK(m_ast.make_u8(), t_u8, t_semi);
+    TEST_OK(m_ast.make_u16(), t_u16, t_semi);
+    TEST_OK(m_ast.make_u32(), t_u32, t_semi);
+    TEST_OK(m_ast.make_u64(), t_u64, t_semi);
+    TEST_OK(m_ast.make_f32(), t_f32, t_semi);
+    TEST_OK(m_ast.make_f64(), t_f64, t_semi);
+
+    TEST_OK(m_ast.make_any_t(), t_any_t, t_semi);
+    TEST_OK(m_ast.make_bool(), t_bool, t_semi);
+    TEST_OK(m_ast.make_type_t(), t_type_t, t_semi);
+    TEST_OK(m_ast.make_void(), t_void, t_semi);
 }
 
 TEST_F(parser, unary) {
@@ -149,117 +161,95 @@ TEST_F(parser, unary) {
 
     TokenDescr t_a{t_id, "a"};
 
-    test_ok({t_amper, t_a, t_semi}, m_ast.make_addr(a));
-    test_ok({t_tilde, t_a, t_semi}, m_ast.make_compl(a));
-    test_ok({t_aster, t_a, t_semi}, m_ast.make_deref(a));
-    test_ok({t_exclam, t_a, t_semi}, m_ast.make_not(a));
-    test_ok({t_return, t_a, t_semi}, m_ast.make_return(a));
-    test_ok({t_minus, t_a, t_semi}, m_ast.make_uminus(a));
-    test_ok({t_plus, t_a, t_semi}, m_ast.make_uplus(a));
+    TEST_OK(m_ast.make_compl(a), t_tilde, t_a, t_semi);
+    TEST_OK(m_ast.make_not(a), t_exclam, t_a, t_semi);
+    TEST_OK(m_ast.make_uminus(a), t_minus, t_a, t_semi);
+    TEST_OK(m_ast.make_uplus(a), t_plus, t_a, t_semi);
+
+    TEST_OK(m_ast.make_addr(a), t_amper, t_a, t_semi);
+    TEST_OK(m_ast.make_deref(a), t_a, t_aster, t_semi);
+
+    TEST_OK(m_ast.make_defer_stmt(a), t_defer, t_a, t_semi);
+
+    TEST_OK(m_ast.make_return(a), t_return, t_a, t_semi);
+
+    TEST_OK(m_ast.make_ptr_type(a), t_aster, t_a, t_semi);
+    TEST_OK(m_ast.make_const_ptr_type(a), t_aster, t_const, t_a, t_semi);
+
+    TEST_OK(m_ast.make_slice_type(a), t_bracket_2x, t_a, t_semi);
 }
 
-// TEST_F(parser, binary) {
-//     auto a = m_ast.make_id(mkt(t_id, "a"));
-//     auto num = m_ast.make_numeric_int(mkt(t_int_const, "42"));
+TEST_F(parser, binary) {
+    auto a = m_ast.make_id(mkt(t_id, "a"));
+    auto num = m_ast.make_numeric_int(mkt(t_int_const, "42"));
 
-//     TokenDescr t_a{t_id, "a"};
-//     TokenDescr t_num{t_int_const, "42"};
+    TokenDescr t_a{t_id, "a"};
+    TokenDescr t_num{t_int_const, "42"};
 
-//     // clang-format off
-//     test_ok({t_num, t_plus, t_num, t_semi},
-//             m_ast.make_add(num, num));
-//     test_ok({t_a, t_plus_eq, t_num, t_semi},
-//             m_ast.make_add_assign(a, num));
-//     test_ok({t_num, t_amper_2x, t_num, t_semi},
-//             m_ast.make_and(num, num));
-//     test_ok({t_a, t_amper_eq, t_num, t_semi},
-//             m_ast.make_and_assign(a, num));
-//     test_ok({t_a, t_eq, t_num, t_semi},
-//             m_ast.make_assign(a, num));
-//     test_ok({t_num, t_amper, t_num, t_semi},
-//             m_ast.make_bitand(num, num));
-//     test_ok({t_num, t_amper_2x_eq, t_num, t_semi},
-//             m_ast.make_bitand_assign(num, num));
-//     test_ok({t_num, t_bar, t_num, t_semi},
-//             m_ast.make_bitor(num, num));
-//     test_ok({t_num, t_bar_2x_eq, t_num, t_semi},
-//             m_ast.make_bitor_assign(num, num));
-//     test_ok({t_cast, t_par_l, t_f64, t_par_r, t_num, t_semi},
-//             m_ast.make_cast(m_ast.make_f64(), num));
-//     test_ok({t_a, t_colon_eq, t_num, t_semi},
-//             m_ast.make_colon_assign(a, num));
-//     test_ok({t_num, t_slash, t_num, t_semi},
-//             m_ast.make_div(num, num));
-//     test_ok({t_a, t_slash_eq, t_num, t_semi},
-//             m_ast.make_div_assign(a, num));
-//     test_ok({t_num, t_eq_2x, t_num, t_semi},
-//             m_ast.make_eq(num, num));
-//     test_ok({t_num, t_greater_eq, t_num, t_semi},
-//             m_ast.make_ge(num, num));
-//     test_ok({t_num, t_greater, t_num, t_semi},
-//             m_ast.make_gt(num, num));
-//     test_ok({t_a, t_bracket_l, t_num, t_bracket_r, t_semi},
-//             m_ast.make_index(a, num));
-//     test_ok({t_num, t_less_eq, t_num, t_semi},
-//             m_ast.make_le(num, num));
-//     test_ok({t_num, t_less_2x, t_num, t_semi},
-//             m_ast.make_lsh(num, num));
-//     test_ok({t_num, t_less_2x_eq, t_num, t_semi},
-//             m_ast.make_lsh_assign(num, num));
-//     test_ok({t_num, t_less, t_num, t_semi},
-//             m_ast.make_lt(num, num));
-//     test_ok({t_num, t_percent, t_num, t_semi},
-//             m_ast.make_mod(num, num));
-//     test_ok({t_a, t_percent_eq, t_num, t_semi},
-//             m_ast.make_mod_assign(a, num));
-//     test_ok({t_num, t_aster, t_num, t_semi},
-//             m_ast.make_mul(num, num));
-//     test_ok({t_a, t_aster_eq, t_num, t_semi},
-//             m_ast.make_mul_assign(a, num));
-//     test_ok({t_num, t_exclam_eq, t_num, t_semi},
-//             m_ast.make_ne(num, num));
-//     test_ok({t_num, t_bar_2x, t_num, t_semi},
-//             m_ast.make_or(num, num));
-//     test_ok({t_a, t_bar_eq, t_num, t_semi},
-//             m_ast.make_or_assign(a, num));
-//     test_ok({t_num, t_greater_2x, t_num, t_semi},
-//             m_ast.make_rsh(num, num));
-//     test_ok({t_a, t_greater_2x_eq, t_num, t_semi},
-//             m_ast.make_rsh_assign(a, num));
-//     test_ok({t_num, t_minus, t_num, t_semi},
-//             m_ast.make_sub(num, num));
-//     test_ok({t_a, t_minus_eq, t_num, t_semi},
-//             m_ast.make_sub_assign(a, num));
-//     test_ok({t_array_t, t_brace_l, t_f64, t_comma, t_num, t_brace_r, t_semi},
-//             m_ast.make_array_type(m_ast.make_f64(), num));
-//     test_ok({t_while, t_num, t_brace_l, t_a, t_semi, t_brace_r},
-//             m_ast.make_while(num, m_ast.make_scope(a)));
-//     test_ok({t_num, t_caret, t_num, t_semi},
-//             m_ast.make_xor(num, num));
-//     test_ok({t_a, t_caret_eq, t_num, t_semi},
-//             m_ast.make_xor_assign(a, num));
-//     // clang-format on
-// }
+    TEST_OK(m_ast.make_add(num, num), t_num, t_plus, t_num, t_semi);
+    TEST_OK(m_ast.make_sub(num, num), t_num, t_minus, t_num, t_semi);
+    TEST_OK(m_ast.make_mul(num, num), t_num, t_aster, t_num, t_semi);
+    TEST_OK(m_ast.make_div(num, num), t_num, t_slash, t_num, t_semi);
+    TEST_OK(m_ast.make_mod(num, num), t_num, t_percent, t_num, t_semi);
 
-// TEST_F(parser, ternary) {
-//     auto a = m_ast.make_id(mkt(t_id, "a"));
-//     auto b = m_ast.make_id(mkt(t_id, "b"));
-//     auto num = m_ast.make_numeric_int(mkt(t_int_const, "42"));
+    TEST_OK(m_ast.make_bitand(num, num), t_num, t_amper, t_num, t_semi);
+    TEST_OK(m_ast.make_bitor(num, num), t_num, t_bar, t_num, t_semi);
+    TEST_OK(m_ast.make_xor(num, num), t_num, t_caret, t_num, t_semi);
+    TEST_OK(m_ast.make_rsh(num, num), t_num, t_greater_2x, t_num, t_semi);
+    TEST_OK(m_ast.make_lsh(num, num), t_num, t_less_2x, t_num, t_semi);
 
-//     TokenDescr t_a{t_id, "a"};
-//     TokenDescr t_b{t_id, "b"};
-//     TokenDescr t_num{t_int_const, "42"};
+    TEST_OK(m_ast.make_and(num, num), t_num, t_amper_2x, t_num, t_semi);
+    TEST_OK(m_ast.make_or(num, num), t_num, t_bar_2x, t_num, t_semi);
 
-//     // clang-format off
-//     test_ok({t_num, t_question, t_a, t_colon, t_b, t_semi},
-//             m_ast.make_ternary(num, a, b));
-//     test_ok({t_if, t_num, t_brace_l, t_a, t_semi, t_brace_r},
-//             m_ast.make_if(num, m_ast.make_scope(a), {}));
-//     test_ok({t_if, t_num, t_brace_l, t_a, t_semi, t_brace_r, t_else, t_brace_l, t_b, t_semi,
-//     t_brace_r},
-//             m_ast.make_if(num, m_ast.make_scope(a), m_ast.make_scope(b)));
-//     // clang-format on
-// }
+    TEST_OK(m_ast.make_eq(num, num), t_num, t_eq_2x, t_num, t_semi);
+    TEST_OK(m_ast.make_ge(num, num), t_num, t_greater_eq, t_num, t_semi);
+    TEST_OK(m_ast.make_gt(num, num), t_num, t_greater, t_num, t_semi);
+    TEST_OK(m_ast.make_le(num, num), t_num, t_less_eq, t_num, t_semi);
+    TEST_OK(m_ast.make_lt(num, num), t_num, t_less, t_num, t_semi);
+    TEST_OK(m_ast.make_ne(num, num), t_num, t_exclam_eq, t_num, t_semi);
+
+    TEST_OK(m_ast.make_add_assign(a, num), t_a, t_plus_eq, t_num, t_semi);
+    TEST_OK(m_ast.make_sub_assign(a, num), t_a, t_minus_eq, t_num, t_semi);
+    TEST_OK(m_ast.make_mul_assign(a, num), t_a, t_aster_eq, t_num, t_semi);
+    TEST_OK(m_ast.make_div_assign(a, num), t_a, t_slash_eq, t_num, t_semi);
+    TEST_OK(m_ast.make_mod_assign(a, num), t_a, t_percent_eq, t_num, t_semi);
+
+    TEST_OK(m_ast.make_bitand_assign(num, num), t_num, t_amper_eq, t_num, t_semi);
+    TEST_OK(m_ast.make_bitor_assign(num, num), t_num, t_bar_eq, t_num, t_semi);
+    TEST_OK(m_ast.make_xor_assign(a, num), t_a, t_caret_eq, t_num, t_semi);
+    TEST_OK(m_ast.make_rsh_assign(a, num), t_a, t_greater_2x_eq, t_num, t_semi);
+    TEST_OK(m_ast.make_lsh_assign(num, num), t_num, t_less_2x_eq, t_num, t_semi);
+
+    // clang-format off
+    TEST_OK(m_ast.make_array_type(m_ast.make_f64(), num),
+        t_bracket_l, t_num, t_bracket_r, t_f64, t_semi);
+    TEST_OK(m_ast.make_cast(m_ast.make_f64(), num),
+            t_cast, t_par_l, t_f64, t_par_r, t_num, t_semi);
+    TEST_OK(m_ast.make_index(a, num),
+            t_a, t_bracket_l, t_num, t_bracket_r, t_semi);
+    TEST_OK(m_ast.make_while(num, m_ast.make_scope(a)),
+        t_while, t_num, t_brace_l, t_a, t_semi, t_brace_r);
+    // clang-format on
+}
+
+TEST_F(parser, ternary) {
+    auto a = m_ast.make_id(mkt(t_id, "a"));
+    auto b = m_ast.make_id(mkt(t_id, "b"));
+    auto num = m_ast.make_numeric_int(mkt(t_int_const, "42"));
+
+    TokenDescr t_a{t_id, "a"};
+    TokenDescr t_b{t_id, "b"};
+    TokenDescr t_num{t_int_const, "42"};
+
+    // clang-format off
+    TEST_OK(m_ast.make_ternary(num, a, b),
+        t_num, t_question, t_a, t_colon, t_b, t_semi);
+    TEST_OK(m_ast.make_if(num, m_ast.make_scope(a), {}),
+        t_if, t_num, t_brace_l, t_a, t_semi, t_brace_r);
+    TEST_OK(m_ast.make_if(num, m_ast.make_scope(a), m_ast.make_scope(b)),
+        t_if, t_num, t_brace_l, t_a, t_semi, t_brace_r, t_else, t_brace_l, t_b, t_semi, t_brace_r);
+    // clang-format on
+}
 
 // TEST_F(parser, other) {
 //     auto id_a = mkt(t_id, "a");
