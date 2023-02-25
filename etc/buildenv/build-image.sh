cbb6bb8fe5d0f1ddaf1864b0e2e59a328adbe3c9
Nick Karter <nkarter@protonmail.com>
2023-02-25 11:52:45 +0200

    Update buildenv to v0.4.1

--- etc/buildenv/build-image.sh
+++ etc/buildenv/build-image.sh
@@ -7,10 +7,9 @@ if [ -z ${PLATFORM+x} ]; then
     PLATFORM=linux
 fi
 
-COMMON_ROOT=$DIR/common
 PLATFORM_ROOT=$DIR/$PLATFORM
 
 . $PLATFORM_ROOT/image-info.sh
 
-cat $COMMON_ROOT/src/Dockerfile $PLATFORM_ROOT/src/Dockerfile |
+cat $PLATFORM_ROOT/src/Dockerfile |
     DOCKER_BUILDKIT=1 docker build -t $IMAGE -f - $DIR
