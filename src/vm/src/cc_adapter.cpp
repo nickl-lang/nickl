a0b2937d7af4e6d5fa92fd96167f0959792a793c
Nick Karter <nkarter@protonmail.com>
2023-03-08 23:32:02 +0200

    Add profiler to nkcc

--- src/vm/src/cc_adapter.cpp
+++ src/vm/src/cc_adapter.cpp
@@ -1,6 +1,7 @@
 #include "cc_adapter.hpp"
 
 #include "nk/common/logger.h"
+#include "nk/common/profiler.hpp"
 #include "nk/common/string_builder.h"
 #include "nk/common/utils.hpp"
 #include "nk/vm/ir.h"
@@ -15,6 +16,9 @@ NK_LOG_USE_SCOPE(cc_adapter);
 } // namespace
 
 std::ostream nkcc_streamOpen(NkIrCompilerConfig const &conf) {
+    EASY_FUNCTION(::profiler::colors::Amber200);
+    NK_LOG_TRC(__func__);
+
     auto sb = nksb_create();
     defer {
         nksb_free(sb);
@@ -36,10 +40,16 @@ std::ostream nkcc_streamOpen(NkIrCompilerConfig const &conf) {
 }
 
 bool nkcc_streamClose(std::ostream const &stream) {
+    EASY_FUNCTION(::profiler::colors::Amber200);
+    NK_LOG_TRC(__func__);
+
     return nk_pipe_streamClose(stream);
 }
 
 bool nkir_compile(NkIrCompilerConfig conf, NkIrProg ir, NkIrFunct entry_point) {
+    EASY_FUNCTION(::profiler::colors::Amber200);
+    NK_LOG_TRC(__func__);
+
     auto src = nkcc_streamOpen(conf);
     nkir_translateToC(ir, entry_point, src);
     return nkcc_streamClose(src);
