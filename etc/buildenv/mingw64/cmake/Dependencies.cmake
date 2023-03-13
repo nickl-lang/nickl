b0ac0829284948af4709b8cfe26b4f2e69bd5958
Nick Karter <nkarter@protonmail.com>
2023-03-13 23:42:20 +0200

    Add system deps to windows

--- etc/buildenv/mingw64/cmake/Dependencies.cmake
+++ etc/buildenv/mingw64/cmake/Dependencies.cmake
@@ -13,42 +13,11 @@ set_target_properties(Dl PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES ${PLATFORM_INCLUDE_DIR}
     )
 
-set(OUT_DIR ${CMAKE_BINARY_DIR}/deps)
-
-file(
-    COPY
-        /usr/lib/gcc/${TOOLCHAIN_PREFIX}/${TOOLCHAIN_VERSISON}/libgcc_s_seh-1.dll
-        /usr/lib/gcc/${TOOLCHAIN_PREFIX}/${TOOLCHAIN_VERSISON}/libssp-0.dll
-        /usr/lib/gcc/${TOOLCHAIN_PREFIX}/${TOOLCHAIN_VERSISON}/libstdc++-6.dll
-        /usr/${TOOLCHAIN_PREFIX}/lib/libwinpthread-1.dll
-    DESTINATION ${OUT_DIR}
-    )
-
-add_custom_command(
-    OUTPUT ${OUT_DIR}/stripped
-    COMMAND ${CMAKE_STRIP} ${OUT_DIR}/libgcc_s_seh-1.dll
-    COMMAND ${CMAKE_STRIP} ${OUT_DIR}/libssp-0.dll
-    COMMAND ${CMAKE_STRIP} ${OUT_DIR}/libstdc++-6.dll
-    COMMAND ${CMAKE_STRIP} ${OUT_DIR}/libwinpthread-1.dll
-    COMMAND ${CMAKE_COMMAND} -E touch ${OUT_DIR}/stripped
-    COMMENT "Stripping MinGW64 dependencies"
-    VERBATIM)
-
-add_custom_target(strip_mingw64_deps ALL
-    DEPENDS ${CMAKE_STRIP} ${OUT_DIR}/libgcc_s_seh-1.dll
-    DEPENDS ${CMAKE_STRIP} ${OUT_DIR}/libssp-0.dll
-    DEPENDS ${CMAKE_STRIP} ${OUT_DIR}/libstdc++-6.dll
-    DEPENDS ${CMAKE_STRIP} ${OUT_DIR}/libwinpthread-1.dll
-    DEPENDS ${OUT_DIR}/stripped
-    )
-
-install(
-    PROGRAMS
-        ${OUT_DIR}/libgcc_s_seh-1.dll
-        ${OUT_DIR}/libssp-0.dll
-        ${OUT_DIR}/libstdc++-6.dll
-        ${OUT_DIR}/libwinpthread-1.dll
-    DESTINATION bin
+set(SYSTEM_DEPS
+    /usr/lib/gcc/${TOOLCHAIN_PREFIX}/${TOOLCHAIN_VERSISON}/libgcc_s_seh-1.dll
+    /usr/lib/gcc/${TOOLCHAIN_PREFIX}/${TOOLCHAIN_VERSISON}/libssp-0.dll
+    /usr/lib/gcc/${TOOLCHAIN_PREFIX}/${TOOLCHAIN_VERSISON}/libstdc++-6.dll
+    /usr/${TOOLCHAIN_PREFIX}/lib/libwinpthread-1.dll
     )
 
 set(PLATFORM_CXX_FLAGS "-fstack-protector")
