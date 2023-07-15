c4b14b252d2253a14628bdb5021e8aca4dd8f8a3
Nick Karter <nkarter@protonmail.com>
2023-07-15 22:01:27 +0300

    Add dllexport for windows

--- src/common/include/nk/common/common.h
+++ src/common/include/nk/common/common.h
@@ -1,6 +1,10 @@
 #ifndef HEADER_GUARD_NK_COMMON_COMMON
 #define HEADER_GUARD_NK_COMMON_COMMON
 
+#ifdef _WIN32
+#define NK_EXPORT __declspec(dllexport)
+#else
 #define NK_EXPORT __attribute__((__visibility__("default")))
+#endif
 
 #endif // HEADER_GUARD_NK_COMMON_COMMON
