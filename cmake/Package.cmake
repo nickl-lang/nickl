72cd787112a80560fffb97405f7cac84f86906ca
Nick Karter <nkarter@protonmail.com>
2025-03-23 16:58:13 +0200

    Use bsdtar for packaging

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -2,18 +2,10 @@ function(add_package_target PKG_NAME OUT_DIR)
     set(PKG_FILENAME "${PKG_NAME}.tar.gz")
     set(PKG_FILE "${OUT_DIR}/${PKG_FILENAME}")
     add_custom_target(package
-        COMMAND ${CMAKE_COMMAND}
-            --build "${CMAKE_BINARY_DIR}"
-            --target $<IF:$<CONFIG:Debug>,install,install/strip>
-            --parallel
         COMMAND mkdir -p "${OUT_DIR}"
-        COMMAND
-            find "${CMAKE_INSTALL_PREFIX}" (-type f -o -type l)
-                -exec realpath --relative-to "${CMAKE_INSTALL_PREFIX}" {} \; |
-            sed -e "s/^\\.\\///g" |
-            xargs tar czf "${PKG_FILE}" --owner=0 --group=0
-                -C "${CMAKE_INSTALL_PREFIX}"
+        COMMAND bsdtar --uid=0 --gid=0 -czvf "${PKG_FILE}" -C "${CMAKE_INSTALL_PREFIX}" .
         COMMAND echo "Generated package '${PKG_FILE}'"
+        DEPENDS $<IF:$<CONFIG:Debug>,install,install/strip>
         COMMENT "Generating package '${PKG_FILENAME}'"
         VERBATIM
         )
