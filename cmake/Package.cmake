87555917223d306b4eaad234ccbf5302f33cb5bc
Nick Karter <nkarter@protonmail.com>
2025-03-24 00:34:36 +0200

    Detect tar version

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -1,9 +1,21 @@
 function(add_package_target PKG_NAME OUT_DIR)
+    execute_process(
+        COMMAND tar --version
+        OUTPUT_VARIABLE TAR_VERSION
+        ERROR_QUIET
+        )
+    if("${TAR_VERSION}" MATCHES "GNU tar")
+        set(TAR_CMD tar --owner=0 --group=0)
+    elseif("${TAR_VERSION}" MATCHES "bsdtar")
+        set(TAR_CMD tar --uid=0 --gid=0)
+    else()
+        message(FATAL_ERROR "Unknown tar version")
+    endif()
     set(PKG_FILENAME "${PKG_NAME}.tar.gz")
     set(PKG_FILE "${OUT_DIR}/${PKG_FILENAME}")
     add_custom_target(package
         COMMAND mkdir -p "${OUT_DIR}"
-        COMMAND bsdtar --uid=0 --gid=0 -czvf "${PKG_FILE}" -C "${CMAKE_INSTALL_PREFIX}" .
+        COMMAND ${TAR_CMD} -czf "${PKG_FILE}" -C "${CMAKE_INSTALL_PREFIX}" .
         COMMAND echo "Generated package '${PKG_FILE}'"
         DEPENDS $<IF:$<CONFIG:Debug>,install,install/strip>
         COMMENT "Generating package '${PKG_FILENAME}'"
