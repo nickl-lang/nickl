010e2e67b7f25b39dc97ba7277d1bb40b6d323c1
Nick Karter <nkarter@protonmail.com>
2025-03-26 19:55:15 +0200

    Reuse argparse logic between buildenv scripts

--- etc/buildenv/config.sh
+++ etc/buildenv/config.sh
@@ -3,6 +3,27 @@
 set -e
 DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 
+print_usage() {
+  echo >&2 "Usage: $0 [-i IMAGE]"
+}
+
+PARSED=$(ARGPARSE_SIMPLE=1 "$DIR/../utils/argparse.sh" "$0" '-h,--help:-i,--image,=' "$@") || {
+  print_usage
+  echo >&2 "Use --help for more info"
+  exit 1
+}
+eval "$PARSED"
+eval set -- "$__POS_ARGS"
+
+[ "$HELP" = 1 ] && {
+  print_usage
+  echo >&2 "Options:"
+  echo >&2 "  -h, --help                          Show this message"
+  echo >&2 "  -i, --image IMAGE=$DEFAULT_IMAGE    Possible values: $IMAGES"
+  echo >&2 ""
+  exit
+}
+
 DEFAULT_IMAGE="$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)"
 [ -z ${IMAGE+x} ] && IMAGE=$DEFAULT_IMAGE
 
--- etc/buildenv/push-image.sh
+++ etc/buildenv/push-image.sh
@@ -3,28 +3,7 @@
 set -e
 DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 
-print_usage() {
-  echo >&2 "Usage: $0 [-i IMAGE]"
-}
-
-PARSED=$(ARGPARSE_SIMPLE=1 "$DIR/../utils/argparse.sh" "$0" '-h,--help:-i,--image,=' "$@") || {
-  print_usage
-  echo >&2 "Use --help for more info"
-  exit 1
-}
-eval "$PARSED"
-eval set -- "$__POS_ARGS"
-
 . "$DIR/config.sh"
-
-[ "$HELP" = 1 ] && {
-  print_usage
-  echo >&2 "Options:"
-  echo >&2 "  -h, --help                          Show this message"
-  echo >&2 "  -i, --image IMAGE=$DEFAULT_IMAGE    Possible values: $IMAGES"
-  echo >&2 ""
-  exit
-}
 
 echo >&2 "INFO: Uploading docker image '$TAG'"
 
