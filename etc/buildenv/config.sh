197faed18471903650bcf66a421cb519a93ae846
Nick Karter <nkarter@protonmail.com>
2025-04-16 19:51:51 +0300

    Add some labels to docker images

--- etc/buildenv/config.sh
+++ etc/buildenv/config.sh
@@ -3,13 +3,23 @@
 set -e
 DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 
+command -v docker >/dev/null 2>&1 || {
+  echo >&2 'ERROR: docker is not installed, use `--native` to build without docker'
+  exit 1
+}
+
+command -v jq >/dev/null 2>&1 || {
+  echo >&2 'ERROR: jq is not installed, need it to parse docker buildenv config'
+  exit 1
+}
+
 print_usage() {
   echo >&2 "Usage: $0 [-i IMAGE]"
 }
 
 DEFAULT_IMAGE="$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)"
 CONFIG=$(cat "$DIR/config.json")
-IMAGES=$(echo "$CONFIG" | jq -r .images[] | cut -d: -f1 | xargs echo)
+IMAGES=$(echo "$CONFIG" | jq -rc .images[] | cut -d: -f1 | xargs echo)
 
 OPTIONS="
   -h, --help                        : Show this message
@@ -26,7 +36,7 @@ eval set -- "$__POS_ARGS"
 
 [ -z ${IMAGE+x} ] && IMAGE=$DEFAULT_IMAGE
 
-IMAGE_CFG_TAG=$(echo "$CONFIG" | jq -r --arg IMAGE "$IMAGE" '.images[] | select(startswith($IMAGE + ":"))')
+IMAGE_CFG_TAG=$(echo "$CONFIG" | jq -rc --arg IMAGE "$IMAGE" '.images[] | select(startswith($IMAGE + ":"))')
 
 [ "$HELP" = 1 ] && {
   print_usage
@@ -42,26 +52,24 @@ IMAGE_CFG_TAG=$(echo "$CONFIG" | jq -r --arg IMAGE "$IMAGE" '.images[] | select(
 SYSTEM=$(echo "$IMAGE" | cut -d- -f1)
 MACHINE=$(echo "$IMAGE" | cut -d- -f2)
 
-BASE_NAME=$(echo "$CONFIG" | jq -r '.base_name')
-REMOTE=$(echo "$CONFIG" | jq -r '.remote')
-ARGS=$(echo "$CONFIG" | jq -r '.args | to_entries[]')
-STAGES=$(echo "$CONFIG" | jq -r '.stages[]')
-DEBUG=$(echo "$CONFIG" | jq -r '.debug_image_build')
-
-ARGS="$ARGS$(cat <<EOF
-
-{
-  "key": "SYSTEM",
-  "value": "$SYSTEM"
-}
-{
-  "key": "MACHINE",
-  "value": "$MACHINE"
-}
-EOF
-)"
+BASE_NAME=$(echo "$CONFIG" | jq -rc '.base_name')
+REMOTE=$(echo "$CONFIG" | jq -rc '.remote')
+ARGS=$(echo "$CONFIG" | jq -rc '.args | to_entries[]')
+STAGES=$(echo "$CONFIG" | jq -rc '.stages[]')
+DEBUG=$(echo "$CONFIG" | jq -rc '.debug_image_build')
 
 IMAGE_NAME="$BASE_NAME-$(echo "$IMAGE_CFG_TAG" | cut -d: -f1)"
 IMAGE_VERSION=$(echo "$IMAGE_CFG_TAG" | cut -d: -f2)
 
 IMAGE_TAG="$IMAGE_NAME:$IMAGE_VERSION"
+
+ARGS="$ARGS$(cat <<EOF
+
+{"key":"SYSTEM","value":"$SYSTEM"}
+{"key":"MACHINE","value":"$MACHINE"}
+{"key":"IMAGE_BASE_NAME","value":"$BASE_NAME"}
+{"key":"IMAGE_NAME","value":"$IMAGE_NAME"}
+{"key":"IMAGE_VERSION","value":"$IMAGE_VERSION"}
+{"key":"IMAGE_CREATED","value":"$(date -u +"%Y-%m-%dT%H:%M:%SZ")"}
+EOF
+)"
