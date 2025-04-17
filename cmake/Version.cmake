f5cb626e185869fe6b3cd0bf1b7840d3829006fa
Nick Karter <nkarter@protonmail.com>
2025-04-18 01:03:46 +0300

    Change deploy package naming a little

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -2,12 +2,11 @@ function(get_build_version REPO_DIR OUT_VERSION)
     find_program(GIT git)
     if(GIT)
         execute_process(
-            COMMAND ${GIT} describe --tags --dirty
+            COMMAND ${GIT} -c safe.directory=* describe --tags --dirty
             WORKING_DIRECTORY ${REPO_DIR}
             RESULT_VARIABLE RESULT
             OUTPUT_VARIABLE VERSION
             OUTPUT_STRIP_TRAILING_WHITESPACE
-            ERROR_QUIET
         )
         if(NOT ${RESULT} EQUAL 0)
             message(WARNING "Repo tag not found")
@@ -16,10 +15,6 @@ function(get_build_version REPO_DIR OUT_VERSION)
     else()
         message(WARNING "Git not found")
         set(VERSION "unknown")
-    endif()
-
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        string(APPEND VERSION "-debug")
     endif()
 
     set(${OUT_VERSION} ${VERSION} PARENT_SCOPE)
